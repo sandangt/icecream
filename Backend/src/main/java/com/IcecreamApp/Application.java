b234b5388748a1f2401258572d802f5207d96d66
San Dang <dangthaisan@hotmail.com>
2020-10-23 17:12:13 +0700

    BE: init data

--- Backend/src/main/java/com/IcecreamApp/Application.java
+++ Backend/src/main/java/com/IcecreamApp/Application.java
@@ -1,7 +1,6 @@
 package com.IcecreamApp;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,4 +8,14 @@ public class Application {
    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+   
+//   @Bean
+//   public CommandLineRunner InitData {DogRepository repository) {
+//
+//	   	return args -> {
+//	   		repository.save(new Dog((long) 1, "ChiHuahua", 99.0));
+//	   		repository.save(new Dog((long) 2, "NgaoDa", 99.0));
+//	   		repository.save(new Dog((long) 3, "NgaoCo", 99.0));
+//	   	}
+//	}
 }
