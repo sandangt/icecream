b46de3aee261ad7ca65e26191a6803a3a2a8e135
San Dang <dangthaisan@hotmail.com>
2020-10-28 23:42:34 +0700

    BE: Signup done

--- Backend/src/main/java/com/IcecreamApp/controller/AuthenticationController.java
+++ Backend/src/main/java/com/IcecreamApp/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.IcecreamApp.controller;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,8 +21,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.IcecreamApp.entity.ERole;
+import com.IcecreamApp.entity.Role;
+import com.IcecreamApp.entity.User;
 import com.IcecreamApp.payload.request.LoginRequest;
+import com.IcecreamApp.payload.request.SignupRequest;
 import com.IcecreamApp.payload.response.JwtResponse;
+import com.IcecreamApp.payload.response.MessageResponse;
 import com.IcecreamApp.repository.RoleRepository;
 import com.IcecreamApp.repository.UserRepository;
 import com.IcecreamApp.security.ApplicationUser;
@@ -74,59 +80,59 @@ public class AuthenticationController {
 		return new ResponseEntity<JwtResponse>(new JwtResponse(jwtToken, appUser.getUsername(), roles), HttpStatus.OK);
 	}
 
-//	@PostMapping("/signup")
-//	public ResponseEntity<MessageResponse> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-//		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-//			return ResponseEntity
-//					.badRequest()
-//					.body(new MessageResponse("Error: Username is already taken!"));
-//		}
-//
-//		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-//			return ResponseEntity
-//					.badRequest()
-//					.body(new MessageResponse("Error: Email is already in use!"));
-//		}
-//
-//		// Create new user's account
-//		User user = new User(signUpRequest.getUsername(), 
-//							 signUpRequest.getEmail(),
-//							 encoder.encode(signUpRequest.getPassword()));
-//
-//		Set<String> strRoles = signUpRequest.getRole();
-//		Set<Role> roles = new HashSet<>();
-//
-//		if (strRoles == null) {
-//			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//			roles.add(userRole);
-//		} else {
-//			strRoles.forEach(role -> {
-//				switch (role) {
-//				case "admin":
-//					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-//							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//					roles.add(adminRole);
-//
-//					break;
-//				case "mod":
-//					Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-//							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//					roles.add(modRole);
-//
-//					break;
-//				default:
-//					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//					roles.add(userRole);
-//				}
-//			});
-//		}
-//
-//		user.setRoles(roles);
-//		userRepository.save(user);
-//
-//		return new ResponseEntity<>(new MessageResponse("User registered successfully!", HttpStatus.OK));
-//	}
+	@PostMapping("/signup")
+	public ResponseEntity<MessageResponse> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Username is already taken!"));
+		}
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Email is already in use!"));
+		}
+
+
+		Set<String> strRoles = signUpRequest.getRole();
+		Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(userRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+				case "admin":
+					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(adminRole);
+
+					break;
+				case "mod":
+					Role modRole = roleRepository.findByName(ERole.ROLE_STAFF)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(modRole);
+
+					break;
+				default:
+					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(userRole);
+				}
+			});
+		}
+
+		// Create new user's account
+		User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()),
+				1,new HashSet<Role>());
+		
+		user.setRoles(roles);
+		userRepository.save(user);
+
+		return new ResponseEntity<MessageResponse>(new MessageResponse("User registered successfully!"), HttpStatus.OK);
+	}
 
 }
--- Backend/src/main/java/com/IcecreamApp/security/ApplicationUserService.java
+++ Backend/src/main/java/com/IcecreamApp/security/ApplicationUserService.java
@@ -3,27 +3,29 @@ package com.IcecreamApp.security;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.IcecreamApp.entity.User;
 import com.IcecreamApp.exception.ResourceNotFoundException;
 import com.IcecreamApp.repository.UserRepository;
-import com.IcecreamApp.utils.PasswordConfig;
 
 @Service
 public class ApplicationUserService implements UserDetailsService {
 
 	private UserRepository userRepository;
+	private PasswordEncoder encoder;
 	
-	public ApplicationUserService(UserRepository userRepository) {
+	public ApplicationUserService(UserRepository userRepository, PasswordEncoder encoder) {
 		this.userRepository = userRepository;
+		this.encoder = encoder;
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userRepository.findByUsername(username)
 				.orElseThrow( () -> new ResourceNotFoundException(String.format("user %s not found", username)));
-		user.setPassword(PasswordConfig.passwordEncoder().encode(user.getPassword()));
+		user.setPassword(encoder.encode(user.getPassword()));
 		ApplicationUser applicationUser = new ApplicationUser(user);
 		return applicationUser;
 	} 
