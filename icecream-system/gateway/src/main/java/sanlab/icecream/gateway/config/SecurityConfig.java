500f4c57013ba25c54ada6eb9f5587dc85ed2e92
San Dang <dangthaisan@hotmail.com>
2023-06-04 13:33:16 +0700

    Update SecurityConfig class

---
+++ icecream-system/gateway/src/main/java/sanlab/icecream/gateway/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package sanlab.icecream.gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import sanlab.icecream.gateway.middleware.JwtAuthConverter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeHttpRequests()
+            .anyRequest()
+            .authenticated();
+        http.oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+}
--- icecream-system/gateway/src/main/java/sanlab/icecream/gateway/controller/WelcomeController.java
+++ icecream-system/gateway/src/main/java/sanlab/icecream/gateway/controller/WelcomeController.java
@@ -1,12 +1,14 @@
 package sanlab.icecream.gateway.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class WelcomeController {
-    @GetMapping("/")
+    @GetMapping("/hello")
+    @PreAuthorize("hasRole('icecream-client-normie')")
     public String sayWelcome() {
         return "<h1>Icecream gateway is up and running</h1>";
     }
